7c7d901a03a173ba0275278b5be31743ede881da
Thomas Rainford <thomas@rainfords.net>
2024-06-09 17:15:30 +1200

    Setup NextJs 14 project with tailwind and DaisyUI plugin.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,8 +15,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html data-theme="dark" lang="en">
+      <body>{children}</body>
     </html>
   );
 }
