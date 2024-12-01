54e31029587ce88c0617f1425c1e46cda6cd85d5
Thomas Rainford <thomas@rainfords.net>
2024-12-01 17:20:11 +1300

    Fix invalid svg prop names.

--- src/app/components/icons.tsx
+++ src/app/components/icons.tsx
@@ -215,9 +215,9 @@ export const PlayIcon = (props: IconProps) => {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     >
       <polygon points="5 3 19 12 5 21 5 3" />
     </svg>
