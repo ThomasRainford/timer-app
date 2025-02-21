ef00d9a5e842ee9f88ee6a918dd088e649e5be10
Thomas Rainford <thomas@rainfords.net>
2025-02-22 10:51:59 +1300

    Fix skeleton styling and form page loading indicators.

--- src/app/series/create/page.tsx
+++ src/app/series/create/page.tsx
@@ -2,14 +2,14 @@ import CreateSeriesForm from "@/app/components/series/create-series/create-serie
 
 const Create = () => {
   return (
-    <div className="flex flex-col justify-center items-center bg-base-200 m-3 p-3 py-4 rounded h-full md:h-auto">
+    <>
       <div className="text-center">
         <h1 className="text-2xl font-bold text-base-content">Create Series</h1>
       </div>
       <div className="w-full md:w-[70%] lg:w-[50%] ">
         <CreateSeriesForm />
       </div>
-    </div>
+    </>
   );
 };
 
--- src/app/series/loading.tsx
+++ src/app/series/loading.tsx
@@ -1,5 +1,9 @@
 const Loading = () => {
-  return <div className="skeleton h-[228px] w-full px-2 md:px-4 mt-2"></div>;
+  return (
+    <div className="">
+      <div className="skeleton  h-[228px] mx-2 md:mx-4 mt-2"></div>
+    </div>
+  );
 };
 
 export default Loading;
