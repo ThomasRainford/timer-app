ba1a947343876564cd3be11f3bd3e4daedf73391
Thomas Rainford <thomas@rainfords.net>
2025-02-22 10:52:36 +1300

    Add layout and loading components for form pages.

---
+++ src/app/series/[id]/timers/create/layout.tsx
@@ -0,0 +1,13 @@
+interface Props {
+  children: React.ReactNode;
+}
+
+const Layout = ({ children }: Props) => {
+  return (
+    <div className="flex flex-col justify-center items-center bg-base-200 m-3 p-3 py-4 rounded h-full md:h-auto">
+      {children}
+    </div>
+  );
+};
+
+export default Layout;
---
+++ src/app/series/[id]/timers/create/loading.tsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <div className="">
+      <span className="loading loading-dots loading-lg"></span>
+    </div>
+  );
+};
+
+export default Loading;
