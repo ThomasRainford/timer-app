fe1582b7a2fedc97c88c60254888f73ddde2a01f
Thomas Rainford <thomas@rainfords.net>
2025-04-21 11:44:46 +1200

    Add loading component for selected series page
    - Upgrade DaisyUI and remove bcryptjs types.

---
+++ src/app/series/[id]/loading.tsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <div className="flex flex-col justify-center items-center mt-10">
+      <span className="loading loading-dots loading-xl"></span>
+    </div>
+  );
+};
+
+export default Loading;
